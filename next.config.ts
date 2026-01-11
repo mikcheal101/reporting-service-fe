7f3003cd8cfe913c92e022660134bf93f2a0918a
mikcheal101 <hirekaanmicheal@gmail.com>
2026-01-11 23:13:11 +0100

    removed the output type

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // Generate export build for Docker
-  output: "export",
-
   images: {
     remotePatterns: [
       {
